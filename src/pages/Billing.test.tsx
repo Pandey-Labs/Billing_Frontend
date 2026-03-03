2ffbccb73f52d899a79b407601ed3d23ba2fe9a8
Pandey-Labs <pandeyaarsh0120@gmail.com>
2026-03-03 17:33:08 +0530

    Fix TypeScript build errors: remove unused fireEvent and renderOptions

--- src/pages/Billing.test.tsx
+++ src/pages/Billing.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
@@ -71,7 +71,6 @@ const renderWithProviders = (
   {
     preloadedState = undefined,
     store = createTestStore(preloadedState),
-    ...renderOptions
   }: any = {}
 ) => {
   function Wrapper({ children }: { children: React.ReactNode }) {
